c4e313a43f468a1d45f7689736dfbc3e644eb263
Imalne <40898021+Imalne@users.noreply.github.com>
2019-12-06 19:22:37 +0800

    multiGPU fix

--- loss/multiCE.py
+++ loss/multiCE.py
@@ -9,12 +9,12 @@ class MultiCrossEntropyLoss(torch.nn.Module):
 
     def forward(self, output, target):
         loss_0 = F.cross_entropy(output[0], target[0],
-                                 weight=torch.FloatTensor(Configs["cross_entropy_weights"]).cuda()) / 64
+                                 weight=torch.FloatTensor(Configs["cross_entropy_weights"]).cuda(output[0].get_device())) / 64
         loss_1 = F.cross_entropy(output[1], target[1],
-                                 weight=torch.FloatTensor(Configs["cross_entropy_weights"]).cuda()) / 16
+                                 weight=torch.FloatTensor(Configs["cross_entropy_weights"]).cuda(output[1].get_device())) / 16
         loss_2 = F.cross_entropy(output[2], target[2],
-                                 weight=torch.FloatTensor(Configs["cross_entropy_weights"]).cuda()) / 4
+                                 weight=torch.FloatTensor(Configs["cross_entropy_weights"]).cuda(output[2].get_device())) / 4
         loss_3 = F.cross_entropy(output[3], target[3],
-                                 weight=torch.FloatTensor(Configs["cross_entropy_weights"]).cuda())
+                                 weight=torch.FloatTensor(Configs["cross_entropy_weights"]).cuda(output[3].get_device()))
         total_loss = loss_0 + loss_1 + loss_2 + loss_3
         return total_loss
